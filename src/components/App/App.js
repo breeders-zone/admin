2ff316aabeffb2651f191ebef92b83cad641a89c
Artrm Poyarkov <poart1405@gmail.com>
2020-06-15 21:30:09 +0300

    add polling in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,38 @@ import AuthLayout from "../../layouts/Auth";
 import {withDataService} from "../hoc";
 import {connect} from "react-redux";
 import {setKinds} from "../../actions/kinds";
+import {setLocalities, setSubcategories} from "../../actions";
 
 class App extends Component {
     componentDidMount() {
+        this.getKindsPooling();
+        this.getSubcategoriesPooling();
+        this.getLocalitiesPooling();
+    }
+
+    getKindsPooling = () => {
         const { getKinds, setKinds } = this.props;
 
         getKinds()
-            .then( (data) => setKinds(data));
-    }
+            .then( (data) => setKinds(data))
+            .catch( () => setTimeout(() => this.getKindsPooling(), 3000));
+    };
+
+    getSubcategoriesPooling = () => {
+        const { getSubcategories, setSubcategories } = this.props;
+
+        getSubcategories()
+            .then( (data) => setSubcategories(data))
+            .catch( () => setTimeout(() => this.getSubcategoriesPooling(), 3000));
+    };
+
+    getLocalitiesPooling = () => {
+        const { getLocalities, setLocalities } = this.props;
+
+        getLocalities()
+            .then( (data) => setLocalities(data))
+            .catch( () => setTimeout(() => this.getLocalitiesPooling(), 3000));
+    };
 
     render() {
         return (
@@ -24,8 +48,10 @@ class App extends Component {
     }
 }
 
-const mapMethodsToProps = ({getKinds}) => ({
-    getKinds
+const mapMethodsToProps = ({getKinds, getSubcategories, getLocalities}) => ({
+    getKinds,
+    getSubcategories,
+    getLocalities
 });
 
-export default connect(null, {setKinds})(withDataService(App, mapMethodsToProps));
+export default connect(null, {setKinds, setSubcategories, setLocalities})(withDataService(App, mapMethodsToProps));
