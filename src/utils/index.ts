102490cafa8dbe642d18c66724e8baef579dba35
Artrm Poyarkov <poart1405@gmail.com>
2020-10-14 15:14:52 +0300

    fix utils

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import toUrl from "./toUrl";
+import getDates from "./getDates";
+import isLogin from "./isLogin";
+import compareMorph from "./compare-morph";
+import toFormData from "./toFormData";
+import times from "./times";
+
+
+export {
+    toUrl,
+    getDates,
+    isLogin,
+    compareMorph,
+    toFormData,
+    times,
+}
